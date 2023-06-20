bf9740b76908a8bbf844b3f69d205d19996e0178
dustin <dnv22@engr.msstate.edu>
2023-06-20 14:01:19 -0500

    finished front

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const numbersContainer = document.getElementsByClassName("numbers");
+const options = document.querySelectorAll('ul.numbers .circle');
+const DEFAULT_BG_COL = 'hsl(213, 9%, 23%)';
+const CLICKED_BG_COL = 'hsl(25, 97%, 53%)';
+const CLICKED_TXT_COL = 'hsl(0, 0%, 100%)';
+const DEFAULT_TXT_COL = 'hsl(217, 12%, 63%)';
+options.forEach(changeColorWhenClicked);
+
+
+function resetCircleColors()
+{
+    options.forEach(function(option) {
+        option.style.backgroundColor = DEFAULT_BG_COL;
+        option.style.color = DEFAULT_TXT_COL
+    })
+}
+
+function changeColorWhenClicked (option)
+    {
+    option.addEventListener('click', function() {
+        resetCircleColors()
+        option.style.backgroundColor = CLICKED_BG_COL
+        option.style.color = CLICKED_TXT_COL
+        });
+    }
